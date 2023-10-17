ffcf6d4ce8f0f6cbdd07c8ea3c8168d352d9da3a
Azenga Kevin <azenga.kevin7@gmail.com>
2023-10-17 04:50:23 +0300

    wip: setting up fontawesome

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import router from '@/router'
 import App from '@/App.vue'
+import '@/fontaweosome'
 import '@/bootstrap'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
